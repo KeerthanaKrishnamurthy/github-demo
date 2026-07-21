e27ce137ba27aeb46464a36a45d10c0b99fd5e7a
RaghuGanti <raghuganti@icloud.com>
2026-07-21 19:28:58 -0400

    Made Mamma Pakodi changes to Uncle.java

--- src/Uncle.java
+++ src/Uncle.java
@@ -3,4 +3,7 @@ public class Uncle extends Animal {
     public void makeSound() {
         System.out.println("Uncle always yells for no reason!");
     }
+    public void MammaPakodi(Animal animal) {
+        System.out.println("Pakodi is being made by Mamma for Uncle");
+    }
 }
