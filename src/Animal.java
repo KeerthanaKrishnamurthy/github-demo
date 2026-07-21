8513208a9cb284b15e74c9ef8934919112dc66ef
Keerthana Krishnamurthy <krishnamurthykeerthana99@gmail.com>
2026-07-21 18:54:24 -0400

    OOPS Example

---
+++ src/Animal.java
@@ -0,0 +1,13 @@
+public class Animal {
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+
+    void sleep() {
+        System.out.println("Animal is sleeping");
+    }
+
+    void makeSound() {
+        System.out.println("Animal makes sound");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+//        System.out.println("Hello, World!");
+//        Dog dog = new Dog();
+//
+//        dog.eat();
+//        dog.sleep();
+//        dog.makeSound();
+//    }
+
+//        //Example 2
+//
+//        Cat cat = new Cat();
+//
+//        dog.makeSound();
+//        cat.makeSound();
+//
+//        makeDogSpeak(dog);
+//
+//        makeCatSpeak(cat);
+//
+//        Dog dog = new Dog();
+//        Cat cat = new Cat();
+        Animal animal = new Dog();
+        makeAnimalSpeak(animal);
+
+        //animal.makeSound();
+
+        animal = new Cat();
+
+        //animal.makeSound();
+
+        makeAnimalSpeak(animal);
+
     }
+//    static void makeDogSpeak(Dog dog) {
+//        dog.makeSound();
+//    }
+//    static void makeCatSpeak(Cat cat) {
+//        cat.makeSound();
+//    }
+
+    static void makeAnimalSpeak(Animal animal) {
+        animal.makeSound();
+    }
+
 }
